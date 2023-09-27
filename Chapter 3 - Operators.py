2d46fc7cfe5978f3461b4c804217d63c0cb3f7fc
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-09-27 14:03:50 +0700

    update

---
+++ Chapter 3 - Operators.py
@@ -0,0 +1,26 @@
+# operasi aritmetika
+
+result = 10 + 30
+result = 40 - 10
+result = 50 * 5
+result = 16 / 4
+result = 25 % 2
+result = 5**3
+
+# Plus-Equal Operator
+
+counter = 0
+counter += 10
+
+# ini bentuk panjangnya
+
+counter = 0
+counter = counter + 10
+
+# Modulo operations
+
+# 8 bagi 4 hasilnya 2 sisa 0 jadi jawabannya 0
+zero = 8 % 4
+
+# 12 bagi 5 hasilnya 10 sisa 2 jadi jawabannya 2
+nonzero = 12 % 5
