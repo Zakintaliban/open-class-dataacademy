e144e483b83d128280cff2abc4c0157bd328f7d6
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-09-28 02:02:44 +0700

    update

--- Chapter 9 - Functions.py
+++ Chapter 9 - Functions.py
@@ -90,29 +90,6 @@ print(three_squared)
 print(four_squared)
 print(five_squared)
 
-# scope variable
-a = 5
-
-
-def f1():
-    a = 2
-    print(a)
-
-
-print(a)  # Will print 5
-f1()  # Will print 2
-
-# Global Variables
-a = "Hello"
-
-
-def prints_a():
-    print(a)
-
-
-# will print "Hello"
-prints_a()
-
 
 # returning value from function
 def check_leap_year(year):
