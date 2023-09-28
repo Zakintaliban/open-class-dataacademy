91323cd16934ef638358532caca25c4f0ece57c3
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-09-28 17:06:20 +0700

    update

---
+++ Chapter 18 - File Operations.py
@@ -0,0 +1,11 @@
+# there are many way to read:
+# a file named "zakin", will be opened with the reading mode.
+file = open("zakin.txt", "r")
+
+# This will print every line one by one in the file
+for each in file:
+    print(each)
+
+# there are many ways to write, but lets move on
+
+# this will write a file named "hadi" with the writing mode.
