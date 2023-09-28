0a00eb3dc234b1f104eb5b2c4b750699a725e68d
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-09-28 17:06:17 +0700

    update

---
+++ Chapter 17 - Virtual Environments & PIP.py
@@ -0,0 +1,40 @@
+# PIP
+
+# PIP is a package manager for Python. It can be used to install, uninstall, and manage Python packages.
+
+# To install a package using PIP, you can use the following command:
+
+# pip install package_name
+
+# To uninstall a package using PIP, you can use the following command:
+
+# pip uninstall package_name
+
+# To see a list of all of the packages that are installed in the current Python environment, you can use the following command:
+
+# pip list
+
+# if there's a requirements.txt file, you can install all the packages in it by using the following command:
+
+# pip install -r requirements.txt
+
+# to create a requirements.txt file, you can use the following command:
+
+# pip freeze > requirements.txt
+
+# create virtual environment
+# Virtual environments are a way to isolate Python packages for different projects. This can be useful for a variety of reasons, such as:
+
+# Avoiding conflicts between different versions of packages
+# Keeping your development environment clean and organized
+# Making it easy to deploy your application to production
+
+# virtualenv venv
+
+# activate virtual environment:
+
+# venv\Scripts\activate
+
+# deactivate virtual environment:
+
+# deactivate
