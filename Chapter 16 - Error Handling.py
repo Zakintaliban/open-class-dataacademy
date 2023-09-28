6a62b91b8b3ccee35c2d206e082598be60f113d1
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-09-28 17:06:15 +0700

    update

---
+++ Chapter 16 - Error Handling.py
@@ -0,0 +1,37 @@
+# Handling Exceptions with Try/Except/Finally
+# put unsafe operation in try block
+try:
+    print("code start")
+
+    # unsafe operation perform
+    print(1 / 0)
+
+# if error occur the it goes in except block
+except:
+    print("an error occurs")
+
+# final code in finally block
+finally:
+    print("GeeksForGeeks")
+
+# Output:
+# code start
+# an error occurs
+# GeeksForGeeks
+
+# Raising exceptions for a predefined condition
+# try for unsafe code
+try:
+    amount = 1999
+    if amount < 2999:
+        # raise the ValueError
+        raise ValueError("please add money in your account")
+    else:
+        print("You are eligible to purchase DSA Self Paced course")
+
+# if false then raise the value error
+except ValueError as e:
+    print(e)
+
+# Output:
+# please add money in your account
