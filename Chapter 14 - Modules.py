c90f1bbedcd3112ccb6559c24905231c16a464e3
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-09-28 22:25:55 +0700

    update

--- Chapter 14 - Modules.py
+++ Chapter 14 - Modules.py
@@ -40,25 +40,3 @@
 # from module import *
 
 # function()
-
-# random.randint() and random.choice()
-# Returns a random integer N in a given range, such that start <= N <= end
-# random.randint(start, end)
-# r1 = random.randint(0, 10)
-# print(r1) # Random integer where 0 <= r1 <= 10
-
-# # Prints a random element from a sequence
-# seq = ["a", "b", "c", "d", "e"]
-# r2 = random.choice(seq)
-# print(r2) # Random element in the sequence
-
-# Module importing
-# file1 content
-# def f1_function():
-# 	  return "Hello World"
-
-# file2
-# import file1
-
-# Now we can use f1_function, because we imported file1
-# f1_function()
